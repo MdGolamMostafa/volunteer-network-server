a62df2a2ab39cd321f04bcc086b315ad89986651
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-06 22:37:56 +0600

    register ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// require('dotenv').config()
+ require('dotenv').config()
 
 const app = express(); 
 app.use(bodyParser.json());
@@ -15,9 +15,22 @@ app.get('/', function(req, res) {
     res.send('Bismillah hirrah manir rahim');
 })
 
+
+
 app.listen(process.env.PORT || 5000);
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useUnifiedTopology: true}, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1});
 client.connect(err => {
-    const productsCollection = client.db("event-task").collection("task");
+    const registersCollection = client.db("event-task").collection("task");
+
+    app.post("/registerInfo",(req, res) => {
+      
+            console.log(req.body);
+             const newRegisterInfo = req.body;
+            registersCollection.insertOne(newRegisterInfo)
+            .then(result => {
+                console.log(result);
+            })
+    })
 });
+
