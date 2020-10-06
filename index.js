fde267415ecd1dfb0aeb48c1feafbae380f9ef4f
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-06 06:08:56 +0600

    first time commit of volunteer-network

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// require('dotenv').config()
+
+const app = express(); 
+app.use(bodyParser.json());
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.cr2af.mongodb.net/
+${process.env.DB_name}?retryWrites=true&w=majority`;
+
+app.get('/', function(req, res) { 
+    res.send('Bismillah hirrah manir rahim');
+})
+
+app.listen(5000);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("event-task").collection("task");
+});
